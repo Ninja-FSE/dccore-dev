6f68bf819c5188e49bfd85345c06879d05bff043
Ninja-FSE <apax0r@gmail.com>
2026-08-26 07:17:53 +0200

    Revert unrelated ADMIN_NICK change picked up from working tree

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ SERVER        = "irc.undernet.org"
 PORT          = 6667
 NICKNAME      = "DCCore"
 ALT_NICKNAME = "DCCore_"
-ADMIN_NICK    = "FLAC,Samoth"
+ADMIN_NICK    = "FLAC"
 CHANNEL       = "#mp3passion,#mp3servers,#mp3-best-of,#mp3country,#mp3albums4u,#mp3download"
 DEBUG_CHANNEL = "#flac-serv"
 
